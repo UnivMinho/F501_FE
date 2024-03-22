aed3d7c24a6838720c5c7b83e8401121ae46fc73
Bruno Afonso <brunoafonso2003@gmail.com>
2024-03-22 05:11:00 +0000

    dashboard vuejs

---
+++ Back_office/src/pages/Notifications/NotificationTemplate.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    Welcome to <b>Black Dashboard</b> - a beautiful freebie for every web
+    developer.
+  </div>
+</template>
+
+<script>
+export default {
+  name: "notification-template",
+};
+</script>
