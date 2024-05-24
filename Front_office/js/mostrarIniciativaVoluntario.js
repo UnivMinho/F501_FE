675287803a880606d5072d5facd4f850c6dbb5ca
mariafaria26 <mariafaria2002@gmail.com>
2024-05-24 17:20:47 +0100

    tabela inscrições

---
+++ Front_office/js/mostrarIniciativaVoluntario.js
@@ -0,0 +1,40 @@
+function showDataSugestoesVoluntarios(){
+    let sugestoes;
+    if(localStorage.getItem("sugestoes")==null){
+      sugestoes = [];
+    }
+    else{
+      sugestoes = JSON.parse(localStorage.getItem("sugestoes"));
+    }
+  
+    let html = "";
+  
+      sugestoes.forEach(function(element, index) {
+        html += "<tr>";
+        html += "<td>" + element.tipo + "</td>";
+        html += "<td>" + element.local + "</td>";
+        html += "<td>" + element.data + "</td>";
+        html += 
+        '<td><button id="inscreverButton' + index + 
+        '" onclick="inscrever(' + index + 
+        ')" style="background: #eec857 border: 2px solid #eec857 color: #fff;">Inscrever</button><span id="inscritoText' + index + 
+        '" style="display:none;">Inscrito</span></td>';
+        html += "</tr>";
+    });
+  
+    const tables = document.querySelectorAll('#sugestoes-table-vol tbody');
+  
+    tables.forEach(table =>{
+      table.innerHTML = html;
+    });
+}
+
+  function inscrever(index) {
+    let button = document.getElementById('inscreverButton' + index);
+    let text = document.getElementById('inscritoText' + index);
+
+    button.style.display = 'none'; // Esconde o botão
+    text.style.display = 'inline'; // Mostra o texto
+}
+
+  document.onload = showDataSugestoesVoluntarios();
