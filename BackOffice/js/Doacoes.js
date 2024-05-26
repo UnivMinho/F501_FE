30d808f1a65125717c9833830c2ba504de7e8e6c
Bruno Afonso <brunoafonso2003@gmail.com>
2024-05-26 16:45:57 +0100

    doacoes bug fix

--- BackOffice/js/Doacoes.js
+++ BackOffice/js/Doacoes.js
@@ -26,14 +26,15 @@ function ArmazenarDoacao() {
         email: email 
     });
 
-    // Calcular o total das doações
-    let fundoManeio = doacoes.reduce((acc, doacao) => acc + doacao.valor, 0);
-
-    // Armazenar o total das doações no localStorage
+    let fundoManeio = parseFloat(localStorage.getItem("fundoManeio") || 0);
+    fundoManeio += valor;
     localStorage.setItem("fundoManeio", fundoManeio);
 
     // Armazenar o array atualizado de doações no localStorage
     localStorage.setItem("doacoes", JSON.stringify(doacoes));
+
+    window.location.reload();
+    alert("Doação realizada com sucesso");
 
     // Retornar false para impedir o envio do formulário
     return false;
