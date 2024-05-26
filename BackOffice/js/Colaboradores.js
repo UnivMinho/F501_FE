a5b92ff15567a2c04dfd9f654e0fe3e4d21fbcd8
Bruno Afonso <brunoafonso2003@gmail.com>
2024-05-26 18:21:22 +0100

    prototipo 2 colaboradores

--- BackOffice/js/Colaboradores.js
+++ BackOffice/js/Colaboradores.js
@@ -4,7 +4,7 @@ function addDataColaboradores(event){
   let nome = document.getElementById("nome").value;
   let email = document.getElementById("email").value;
   let role = document.getElementById("role").value;
-  let id = darID();
+  let id = darIDColab();
 
   let colaboradores;
   if(localStorage.getItem("colaboradores")==null){
@@ -50,14 +50,63 @@ function showDataColaboradores(){
 }
 
 function updateColaborador(id){
+    // Recupera as colaboradores do localStorage
+  let colaboradores = JSON.parse(localStorage.getItem("colaboradores")) || [];
 
+  let index = colaboradores.findIndex(iniciativa => iniciativa.id === id);
+
+  let form = document.getElementById("form-popup");
+
+  if(index !== -1){
+      let colaboradorSelecionado = colaboradores[index];
+
+      form.elements["nome"].value = colaboradorSelecionado.nome;
+      form.elements["email"].value = colaboradorSelecionado.email;
+      form.elements["role"].value = colaboradorSelecionado.role;
+
+      showPopupColaboradores();
+
+      form.addEventListener("submit", function(event) {
+          event.preventDefault();
+
+          // Obtém os novos detalhes da iniciativa a partir do formulário
+          let novosDetalhes = {
+              id: id,
+              nome: form.elements["nome"].value,
+              email: form.elements["email"].value,
+              role: form.elements["role"].value,
+          };
+
+          colaboradores = colaboradores.map(item => item.id === id ? novosDetalhes : item);
+
+          // Atualiza o localStorage com as colaboradores atualizadas
+          localStorage.setItem("colaboradores", JSON.stringify(colaboradores));
+
+          showDataColaboradores();
+          hidePopupColaboradores();
+          window.location.reload();
+
+      }, { once: true }); // Adiciona o evento somente uma vez para evitar múltiplos handlers
+  }
 }
 
 function deleteColaboradores(id){
-
+    
 }
 
 
-function showPopupColaboradores(){}
+function showPopupColaboradores(){
+    document.getElementById('popup-background-colaboradores-editar').style.display = 'flex';
+}
 
-function hidePopupColaboradores(){}
+function hidePopupColaboradores(){
+    document.getElementById('popup-background-colaboradores-editar').style.display = 'none';
+}
+
+
+function darIDColab() {
+    let id = localStorage.getItem("idcolab") ? JSON.parse(localStorage.getItem("idcolab")) : 0;
+    id += 1;
+    localStorage.setItem("idcolab", JSON.stringify(id));
+    return id;
+  }
